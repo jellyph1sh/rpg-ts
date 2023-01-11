1fd10b2fd9ec327e757653f3822fc5430e13f6b9
laremi <91208870+Godlonx@users.noreply.github.com>
2023-01-11 14:14:30 +0100

    :white_check_mark: | First version of the Inventory class

--- classes/Inventory.ts
+++ classes/Inventory.ts
@@ -1,2 +1,24 @@
 export class Inventory {
+    Items:Item[] = [];
+
+    AddItem(item:Item):void {
+        this.Items.push(item)
+    }
+
+    RemoveItem(itemIndex:number):void {
+        this.Items.splice(itemIndex, 1)
+    }
+
+    ShowInventory() {
+        console.log(this.Items)
+    }
+}
+
+class Item {
+    Effect:string = "";
+    Amount:number = 1;
+
+    constructor(effect:string){
+        this.Effect = effect;
+    }
 }
