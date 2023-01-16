7d48d3cdc614b0653d3ebfdd3a1696c7d21c0179
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-01-16 15:56:02 +0100

    :white_check_mark: | Working keyboard control feature added!

--- classes/Menu.ts
+++ classes/Menu.ts
@@ -1,10 +1,10 @@
 import {Color} from "./Color.ts";
 
 export class Menu {
-    title:string;
-    description:string;
-    buttons:string[];
-    input:number = 0;
+    private title:string;
+    private description:string;
+    private buttons:string[];
+    private input = 0;
 
     constructor(title:string, description:string, buttons:string[]) {
         this.title = title;
@@ -12,40 +12,48 @@ export class Menu {
         this.buttons = buttons;
     }
 
-    up() {
+    private Up():void {
         if (this.input > 0) {
             this.input -= 1;
         }
     }
 
-    down() {
+    private Down():void {
         if (this.input !== this.buttons.length - 1) {
             this.input += 1;
         }
     }
 
-    Naviguate() {
+    public Naviguate():number {
+        /*  Up : 27 91 65 
+            Down : * * 66
+            Enter : 13 0 0*/
+        
         let enter = false;
         while (!enter) {
-            this.display();
-            let consoleInput = window.prompt("Choice >> ")
-            if (consoleInput === "up") {
-                this.up();
-            } else if (consoleInput === "down") {
-                this.down();
-            } else if (consoleInput === "enter") {
+            this.Display();
+            const buf = new Uint8Array(3);
+            Deno.stdin.setRaw(true, { cbreak: true });
+            Deno.stdin.readSync(buf);
+            Deno.stdin.setRaw(false);
+            if (buf[0] === 27 && buf[1] === 91, buf[2] === 65) {
+                this.Up();
+            } else if (buf[0] === 27 && buf[1] === 91, buf[2] === 66) {
+                this.Down();
+            } else if (buf[0] === 13 && buf[1] === 0, buf[2] === 0) {
                 enter = !enter;
             }
         }
+        
         return this.input;
     }
 
-    display():void {
+    private Display():void {
         console.clear();
         console.log(this.title);
         console.log(this.description);
         for(let i=0; i < this.buttons.length; i++) {
-            let text = `${i+1}. ${this.buttons[i]}.`;
+            const text = `${i+1}. ${this.buttons[i]}.`;
             let color = Color.white;
             if (this.input === i) {
                 color = Color.red;
@@ -54,6 +62,3 @@ export class Menu {
         }
     }
 }
-
-let menuTest = new Menu("Test", "Description de test", ["Rémi", "Emeric", "Sacha"]);
-console.log(menuTest.Naviguate());
