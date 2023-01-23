28d400e6dc1e15758b277186fa9fdb8ac88ef97b
laremi <91208870+Godlonx@users.noreply.github.com>
2023-01-23 16:20:46 +0100

    :white_check_mark: | Add working version of floor and room with procedural generation of the floor

---
+++ classes/Room.ts
@@ -0,0 +1,24 @@
+export class Room {
+    roomType:RoomType;
+    position:number;
+
+    constructor(typeZone:RoomType, position:number) {
+        this.roomType = typeZone
+        this.position = position
+    }
+}
+
+export enum RoomType {
+    Void = 1,
+    Fight = 2,
+    Chest = 3,
+    TrapChest = 4,
+    BossFight = 5
+}
+
+export enum Orientation {
+    Up = -5,
+    Down = 5,
+    Left = -1,
+    Right = 1
+}
